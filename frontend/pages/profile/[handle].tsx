6bb6381036ba28ef81cfc0c330503cdaecbb27d1
DavNej <david.nejar@gmail.com>
2023-02-16 12:42:08 +0100

    frontend | single profile view with NFT display

--- frontend/pages/profile/[handle].tsx
+++ frontend/pages/profile/[handle].tsx
@@ -1,29 +1,63 @@
 import * as React from 'react'
 import { useRouter } from 'next/router'
-import { Flex, Heading, Image } from '@chakra-ui/react'
 
-import { useStore } from '@/hooks/useStore'
+import { Box, Flex, Heading, Image } from '@chakra-ui/react'
+
+import { useProfile } from '@/hooks/useProfile'
+
+function decodeJsonDataURI(dataURI: string) {
+  const base64String = dataURI.replace('data:application/json;base64,', '')
+  const buffer = Buffer.from(base64String, 'base64')
+  const jsonString = buffer.toString()
+
+  return JSON.parse(jsonString)
+}
+
+function decodeImageDataURI(dataURI: string) {
+  const base64String = dataURI.replace('data:image/svg+xml;base64,', '')
+  const buffer = Buffer.from(base64String, 'base64')
+  const svg = buffer.toString()
+
+  return svg
+}
 
 export default function Profile() {
   const {
     query: { handle },
   } = useRouter()
 
-  const { store } = useStore()
+  const profileHandle = handle ? handle.toString() : null
+  const profile = useProfile(profileHandle)
 
-  const profile = store[handle]
+  if (!profile) return null
+
+  const NFTJson = decodeJsonDataURI(profile.tokenURI)
+  const image = decodeImageDataURI(NFTJson.image)
 
   return (
-    <Flex alignItems='center' bg='white' p={4} borderRadius='xl'>
-      <Image
-        borderRadius='full'
-        boxSize='150px'
-        src={profile.imageURI}
-        alt={`@${profile.handle}`}
-      />
-      <Heading ml={8} size='md'>
-        @{profile.handle}
-      </Heading>
-    </Flex>
+    <Box bg='white' p={4} borderRadius='xl'>
+      {profile && (
+        <Box>
+          <Flex alignItems='center'>
+            <Image
+              borderRadius='full'
+              boxSize='150px'
+              src={profile.imageURI}
+              alt={`@${profile.handle}`}
+            />
+            <Heading ml={8} size='md'>
+              @{profile.handle}
+            </Heading>
+          </Flex>
+          <Flex p={8} flexDirection='column' alignItems='center'>
+            <Heading ml={8} size='md'>
+              Profile NFT
+            </Heading>
+
+            <div dangerouslySetInnerHTML={{ __html: image }} />
+          </Flex>
+        </Box>
+      )}
+    </Box>
   )
 }
