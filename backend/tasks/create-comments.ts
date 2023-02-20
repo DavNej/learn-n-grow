f1d8fa146b9611bd361f420f32feac7e29b95f97
DavNej <david.nejar@gmail.com>
2023-02-20 07:26:28 +0100

    backend | add create-comments task

--- backend/tasks/create-comments.ts
+++ backend/tasks/create-comments.ts
@@ -5,33 +5,40 @@ import { LearnNGrow__factory } from '../typechain-types/index'
 import { waitForTx } from '../utils'
 import { CONTRACT_ADDRESS } from '../utils/constants'
 
-task('create-posts', 'Populate posts').setAction(async (_, hre) => {
+task('create-comments', 'Populate comments').setAction(async (_, hre) => {
   const [deployer, user, userTwo, userThree] = await hre.ethers.getSigners()
 
   const learnNGrow = LearnNGrow__factory.connect(CONTRACT_ADDRESS, deployer)
 
   const userProfileId = await learnNGrow.profile(user.address)
+  const userTwoProfileId = await learnNGrow.profile(userTwo.address)
+  const userThreeProfileId = await learnNGrow.profile(userThree.address)
+
   await waitForTx(
-    learnNGrow.connect(user).post({
+    learnNGrow.connect(user).comment({
       profileId: userProfileId,
+      profileIdPointed: userTwoProfileId,
+      pubIdPointed: hre.ethers.BigNumber.from(1),
       contentURI:
         'https://gateway.pinata.cloud/ipfs/bafkreibddjzdanba2sz3r5habkk2obtfpgf2baqnn3sjc2drrtpwt22kae',
     })
   )
 
-  const userTwoProfileId = await learnNGrow.profile(userTwo.address)
   await waitForTx(
-    learnNGrow.connect(userTwo).post({
+    learnNGrow.connect(userTwo).comment({
       profileId: userTwoProfileId,
+      profileIdPointed: userTwoProfileId,
+      pubIdPointed: hre.ethers.BigNumber.from(1),
       contentURI:
-        'https://gateway.pinata.cloud/ipfs/bafkreigkbol2sdv2zzx7gkgooephmt6jftk5g5jtmb7gkk2qmjxwxegwhq',
+        'https://gateway.pinata.cloud/ipfs/bafkreibddjzdanba2sz3r5habkk2obtfpgf2baqnn3sjc2drrtpwt22kae',
     })
   )
 
-  const userThreeProfileId = await learnNGrow.profile(userThree.address)
   await waitForTx(
-    learnNGrow.connect(userThree).post({
+    learnNGrow.connect(userThree).comment({
       profileId: userThreeProfileId,
+      profileIdPointed: userTwoProfileId,
+      pubIdPointed: hre.ethers.BigNumber.from(1),
       contentURI:
         'https://gateway.pinata.cloud/ipfs/bafkreidwxv2fwzp26rrhm43r3ii5xvlgvokbqgvfaevm5qsnqhufzlrjhe',
     })
