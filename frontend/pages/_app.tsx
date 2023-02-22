83aa0bc6df3939366bb96159061b0bb8cadafdb7
DavNej <david.nejar@gmail.com>
2023-02-22 14:13:26 +0100

    frontend | react-query setup

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,17 +5,32 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import RainbowKitProvider from '@/utils/rainbowkit'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import Layout from '@/components/Layout'
 import { StoreProvider } from '@/hooks/useStore'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RainbowKitProvider>
       <ChakraProvider>
         <StoreProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <QueryClientProvider client={queryClient}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </StoreProvider>
       </ChakraProvider>
     </RainbowKitProvider>
